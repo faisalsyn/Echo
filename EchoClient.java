6e2b565f5be34d9c7b017303549c47e312bf11ae
nawab <nawab@hamilton.(none)>
2012-06-01 12:08:04 -0700

    F

--- EchoClient.java
+++ EchoClient.java
@@ -36,7 +36,7 @@ public class EchoClient
 		
 		try
 		{
-			Socket s = new Socket("127.0.0.1", 9999);
+			Socket s = new Socket(args[1], 9999);
 			BufferedReader r = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			PrintWriter w = new PrintWriter(s.getOutputStream(), true);
 			BufferedReader con = new BufferedReader(new InputStreamReader(System.in));
