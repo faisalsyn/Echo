4eec75019e2de07646e4b8fc65fd43917e1e06aa
nawab <nawab@hamilton.(none)>
2012-06-01 13:02:56 -0700

    F

--- set-env.sh
+++ set-env.sh
@@ -1,3 +1,6 @@
-v1=10.249.185.221
-v2=10.120.179.56
-o=10.244.0.114
+v1l=10.249.185.221
+v2l=10.120.179.56
+v1='ec2-23-22-102-174.compute-1.amazonaws.com'
+v2='ec2-23-22-156-183.compute-1.amazonaws.com'
+o='ec2-50-112-28-90.us-west-2.compute.amazonaws.com'
+c='ec2-50-18-37-161.us-west-1.compute.amazonaws.com'
